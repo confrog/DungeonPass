5d049157e0c31caa384ede179b0fbfe67da8c11d
confrog <confrog.central@gmail.com>
2017-05-07 16:28:59 -0400

    Update testing_grounds.lua

--- testing_grounds.lua
+++ testing_grounds.lua
@@ -7,8 +7,24 @@ end
 require "entity"
 require "item"
 require "combat"
+require "tile"
 --
 --
+longsword = item.Weapon:new()
+  longsword.name = "Longsword"
+  longsword.atk = 1
+  longsword.dmg = 3
+chainmail = item.Armor:new()
+  chainmail.name = "Chainmail Armor"
+  chainmail.defense = 3
+
+player = entity.Entity:new()
+  player.name = "Player"
+  player.inventory[1] = longsword
+  player.inventory[2] = chainmail
+  player.equipped.weapon = player.inventory[1]
+  player.equipped.armor = player.inventory[2]
+  player:equip_update()
 --
 gerblin_sword = item.Weapon:new()
   gerblin_sword.name = "Gerblin Sword"
@@ -29,4 +45,11 @@ gerblin:equip_update()
 slime = entity.Entity:new()
 slime.name = "Slime"
 
-combat.fight(gerblin,slime)
+test_room = tile.CombatTile:new()
+  test_room.monster = gerblin
+  test_room.name = "Test Chamber"
+  test_room.room_desc = "\tA blank room"
+  test_room.is_complete = false
+  test_room.rspwn_chance = 50
+
+test_room:enter_tile(player)
