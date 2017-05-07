22c667feeba6821aa0e6ca1eb97b432ff659169d
confrog <confrog.central@gmail.com>
2017-05-07 16:28:26 -0400

    Tile
    Contains code for tiles

---
+++ tile.lua
@@ -0,0 +1,30 @@
+require "combat"
+
+tile = {}
+--
+tile.CombatTile = {}
+  tile.CombatTile.monster = nil;
+  tile.CombatTile.name = "A Fighting Room";
+  tile.CombatTile.room_desc = "None";
+  tile.CombatTile.is_complete = false;
+  tile.CombatTile.rspwn_chance = 0
+function tile.CombatTile:new(o)
+  o = o or {}
+  setmetatable(o,self)
+  self.__index = self
+  return o
+end
+function tile.CombatTile:enter_tile(player)
+  print(self.name)
+  print(self.room_desc)
+  if self.is_complete == false then
+    combat.fight(player, self.monster)
+  else
+    math.randomseed(os.time()) --> create respawn chance function and value for combat tiles
+    if math.random(100) <= self.rspwn_chance then
+      combat.fight(player, self.monster)
+    else
+      print("The room is empty")
+    end
+  end
+end
