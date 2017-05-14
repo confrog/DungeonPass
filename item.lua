48383e4f16e5924a199938e102847efe63da6f4e
confrog <confrog.central@gmail.com>
2017-05-14 18:07:13 -0400

    Update item.lua

--- item.lua
+++ item.lua
@@ -1,5 +1,5 @@
 item = {}
--- Equipment --
+--
 item.Weapon = {name = nil, atk = 0, dmg = 0}
 function item.Weapon:new(o)
   o = o or {}
@@ -15,14 +15,45 @@ function item.Armor:new(o)
   self.__index = self
   return o
 end
--- end Equipment --
 --
+--> Item Library <--
+-- Weapons --
 unarmed = item.Weapon:new()
   unarmed.name = "Unarmed"
   unarmed.atk = 0
   unarmed.dmg = 1
+longsword = item.Weapon:new()
+  longsword.name = "Longsword"
+  longsword.atk = 1
+  longsword.dmg = 3
+dirk = item.Weapon:new()
+  dirk.name = "Dirk"
+  dirk.atk = 1
+  dirk.dmg = 2
+staff = item.Weapon:new()
+  staff.name = "Staff"
+  staff.atk = 1
+  staff.dmg = 1
+bastard_sword = item.Weapon:new()
+  bastard_sword.name = "Bastard Sword"
+  bastard_sword.atk = 2
+  bastard_sword.dmg = 4
+
+-- Armor --
 naked = item.Armor:new()
   naked.name = "Naked"
   naked.defense = 0
+thief_armor = item.Armor:new()
+  thief_armor.name = "Thief's Armor"
+  thief_armor.defense = 1
+merc_armor = item.Armor:new()
+  merc_armor.name = "Mercenary Armor"
+  merc_armor.defense = 2
+robes = item.Armor:new()
+  robes.name = "Robes"
+  robes.defense = 0
+chainmail = item.Armor:new()
+  chainmail.name = "Chainmail Armor"
+  chainmail.defense = 3
 --
 return item
